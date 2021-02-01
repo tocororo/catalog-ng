5f459a96a802eb5555ae16fe4174bd160e9479bf
Rafael Martinez Estevez <rafael.martinez@upr.edu.cu>
2021-02-01 15:30:11 -0500

    install

--- projects/catalog/src/environments/environment.ts
+++ projects/catalog/src/environments/environment.ts
@@ -6,10 +6,10 @@ import { Environment } from 'toco-lib';
 
 class EnvironmentImpl implements Environment {
   production = false;
-  sceibaHost = 'https://localhost:5000/';
-  cuorHost = 'https://localhost:5001/';
-  sceibaApi = 'https://localhost:5000/api/';
-  cuorApi = 'https://localhost:5001/api/';
+  sceibaHost = 'https://10.2.83.214:5000/';
+  cuorHost = 'https://10.2.83.214:5001/';
+  sceibaApi = 'https://10.2.83.214:5000/api/';
+  cuorApi = 'https://10.2.83.214:5001/api/';
 
   appHost = 'https://127.0.0.1:4200';
   appName = 'Sceiba Catalogo';
