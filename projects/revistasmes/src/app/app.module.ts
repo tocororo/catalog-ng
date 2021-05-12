7a1ba62cb046b3eb3f940dfddbc403938fcd456e
acm-97 <alejandro97.acm@gmail.com>
2021-05-12 16:12:19 -0400

    adding keyloak

--- projects/revistasmes/src/app/app.module.ts
+++ projects/revistasmes/src/app/app.module.ts
@@ -1,25 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { HomeRevistasmesComponent } from './home/home.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OAuthModule, OAuthStorage } from 'angular-oauth2-oidc';
+import { MarkdownModule } from 'ngx-markdown';
+import { CatalogAppModule } from 'projects/catalog/src/app/app.module';
+import { DialogCatalogJournalInfoDialog } from 'projects/catalog/src/app/catalog/catalog.component';
 import { MySourcesManagerPermissionDialog } from 'projects/catalog/src/app/mysources/manager/manager.component';
 import { MySourcesOrganizationsPermissionDialog } from 'projects/catalog/src/app/mysources/organizations/organizations.component';
 import { MySourcesTermsPermissionDialog } from 'projects/catalog/src/app/mysources/terms/terms.component';
 import { SourceEditAddIndexComponent } from 'projects/catalog/src/app/source-edit/source-indexes/source-indexes.component';
-import { SourceEditOrganizationSelectTopDialog, SourceEditOrganizationSelectDialog } from 'projects/catalog/src/app/source-edit/source-organizations/source-organizations.component';
+import { SourceEditOrganizationSelectDialog, SourceEditOrganizationSelectTopDialog } from 'projects/catalog/src/app/source-edit/source-organizations/source-organizations.component';
 import { SourceInclusionAcceptComponent } from 'projects/catalog/src/app/source-inclusion/source-inclusion.component';
 import { SourceViewSaveDialog } from 'projects/catalog/src/app/source-view/source-view.component';
-import { DialogCatalogJournalInfoDialog } from 'projects/catalog/src/app/catalog/catalog.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { NotificationModule, TocoFormsModule, OrganizationsModule, CoreModule, SharedModule, AuthenticationModule, AngularMaterialModule, SourceService, SourceServiceNoAuth, UserProfileService, TaxonomyService, OrganizationServiceNoAuth, HTTP_INTERCEPTOR_PROVIDERS, Environment } from 'toco-lib';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MarkdownModule } from 'ngx-markdown';
-import { HttpClient } from '@angular/common/http';
-import { CatalogAppModule } from 'projects/catalog/src/app/app.module';
+import { AngularMaterialModule, AuthenticationModule, CoreModule, Environment, HTTP_INTERCEPTOR_PROVIDERS, NotificationModule, OrganizationServiceNoAuth, OrganizationsModule, SharedModule, SourceService, SourceServiceNoAuth, TaxonomyService, TocoFormsModule, UserProfileService } from 'toco-lib';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeRevistasmesComponent } from './home/home.component';
 
-
+export function storageFactory() : OAuthStorage {
+  return sessionStorage
+}
 
 @NgModule({
   declarations: [
@@ -54,6 +57,7 @@ import { environment } from '../environments/environment';
     MarkdownModule.forRoot({
       loader: HttpClient
     }),
+    OAuthModule.forRoot(),
     CatalogAppModule
   ],
   providers: [
@@ -65,7 +69,8 @@ import { environment } from '../environments/environment';
     HTTP_INTERCEPTOR_PROVIDERS,
     // REQUEST_CACHE_DIFFERENT_TIME_WITH_MAP_PROVIDER,
     // { provide: HTTP_INTERCEPTORS, useClass: OauthAuthenticationService, multi: true },
-    { provide: Environment, useValue: environment }
+    { provide: Environment, useValue: environment },
+    { provide: OAuthStorage, useFactory: storageFactory },
   ],
   bootstrap: [AppComponent]
 })
